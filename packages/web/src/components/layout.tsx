8b0fba582b31d2e8d6596890fe2e5a701b5cc983
aereal <aereal@aereal.org>
2019-06-06 13:19:30 +0900

    web: extract DrawerMenu

--- packages/web/src/components/layout.tsx
+++ packages/web/src/components/layout.tsx
@@ -1,17 +1,15 @@
 import AppBar from "@material-ui/core/AppBar"
 import Container from "@material-ui/core/Container"
 import CssBaseline from "@material-ui/core/CssBaseline"
-import Drawer from "@material-ui/core/Drawer"
 import Grid from "@material-ui/core/Grid"
 import IconButton from "@material-ui/core/IconButton"
 import Link from "@material-ui/core/Link"
 import { makeStyles } from "@material-ui/core/styles"
 import Toolbar from "@material-ui/core/Toolbar"
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft"
 import MenuIcon from "@material-ui/icons/Menu"
 import React, { FC, useState } from "react"
 import { routes } from "../routes"
-import { DrawerMenuList } from "./drawer-menu-list"
+import { DrawerMenu } from "./drawer-menu"
 
 const drawerWidth = 240
 
@@ -41,26 +39,6 @@ const useStyles = makeStyles(theme => ({
     height: "100vh",
     overflow: "auto",
   },
-  drawerPaper: {
-    position: "relative",
-    transition: theme.transitions.create("width", {
-      duration: theme.transitions.duration.enteringScreen,
-      easing: theme.transitions.easing.sharp,
-    }),
-    whiteSpace: "nowrap",
-    width: drawerWidth,
-  },
-  drawerPaperClose: {
-    overflowX: "hidden",
-    transition: theme.transitions.create("width", {
-      duration: theme.transitions.duration.leavingScreen,
-      easing: theme.transitions.easing.sharp,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9),
-    },
-  },
   menuButton: {
     marginRight: 36,
   },
@@ -73,13 +51,6 @@ const useStyles = makeStyles(theme => ({
   toolbar: {
     paddingRight: 24,
   },
-  toolbarIcon: {
-    alignItems: "center",
-    display: "flex",
-    justifyContent: "flex-end",
-    padding: "0 8px",
-    ...theme.mixins.toolbar,
-  },
 }))
 
 const cls = (classes: Array<string | boolean>): string =>
@@ -89,7 +60,6 @@ export const Layout: FC = ({ children }) => {
   const classes = useStyles()
   const [openDrawer, setOpenDrawer] = useState<boolean>(false)
   const handleDrawerOpen = () => setOpenDrawer(true)
-  const handleDrawerClose = () => setOpenDrawer(false)
 
   return (
     <div className={classes.root}>
@@ -117,23 +87,7 @@ export const Layout: FC = ({ children }) => {
           </Link>
         </Toolbar>
       </AppBar>
-      <Drawer
-        variant="permanent"
-        open={openDrawer}
-        classes={{
-          paper: cls([
-            classes.drawerPaper,
-            !openDrawer && classes.drawerPaperClose,
-          ]),
-        }}
-      >
-        <div className={classes.toolbarIcon}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <DrawerMenuList />
-      </Drawer>
+      <DrawerMenu open={openDrawer} setOpen={setOpenDrawer} />
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
