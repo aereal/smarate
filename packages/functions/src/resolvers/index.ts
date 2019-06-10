1b16cf4568385dc0cddb9e21efc34c4836807169
aereal <aereal@aereal.org>
2019-06-11 01:33:55 +0900

    functions: fighter win ratio

--- packages/functions/src/resolvers/index.ts
+++ packages/functions/src/resolvers/index.ts
@@ -2,6 +2,7 @@ import { GetDB } from "../firebase"
 import { buildFightResultResolver } from "./fight-result"
 import { buildFighterResolver } from "./fighter"
 import { buildFighterFightResultResolver } from "./fighter-fight-result"
+import { buildFighterFightResultConnection } from "./fighter-fight-result-connection"
 import { buildMutationResolver } from "./mutation"
 import { buildQueryResolver } from "./query"
 import { buildUserResolver } from "./user"
@@ -10,6 +11,7 @@ export const buildResolver = (getDB: GetDB) => ({
   FightResult: buildFightResultResolver(),
   Fighter: buildFighterResolver(getDB),
   FighterFightResult: buildFighterFightResultResolver(),
+  FighterFightResultConnection: buildFighterFightResultConnection(),
   Mutation: buildMutationResolver(getDB),
   Query: buildQueryResolver(getDB),
   User: buildUserResolver(getDB),
