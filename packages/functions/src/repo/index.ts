3fb3d0eba5d703ad22fc1297cfdc648ffe574572
aereal <aereal@aereal.org>
2019-07-07 17:05:06 +0900

    remove functions

--- packages/functions/src/repo/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./fight-result-repo"
-export * from "./user-preference-repo"
-export * from "./fighter"
-export * from "./stats"
--- packages/functions/src/resolvers/fighter-fight-result.ts
+++
@@ -1,6 +0,0 @@
-import { Timestamp } from "@google-cloud/firestore"
-
-export const buildFighterFightResultResolver = () => ({
-  recordedAt: (parent: { recordedAt: Timestamp }) =>
-    parent.recordedAt.toDate().toISOString(),
-})
